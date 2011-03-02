391cb0ae8830d21899cb8116bced4348fbb29248
Davi Ferreira <contato@daviferreira.com>
2011-03-02 16:32:23 -0300

    Re-factor

---
+++ README.markdown
@@ -0,0 +1,16 @@
+MsgTopo
+=======
+
+Função Javascript/Jquery para exibir uma mensagem no topo da página.
+
+Usando...
+---------
+
+		msgTopo('Mensagem de erro', 'erro', 5);
+		msgTopo('Mensagem padrão');
+		msgTopo('Sucesso!', 'sucesso', 4);
+
+A função recebe 3 parâmetros:
+	* A mensagem a ser exibida
+	* O tipo de mensagem [erro/sucesso]
+	* O tempo em segundos para exibição da mensagem
--- msgtopo.js
+++ msgtopo.js
@@ -1,51 +1,34 @@
-// timer para o timeout do javascript
 var msgTopoTimer = '';
 
-// função simples para exibir resposta de erro ou confirmação
-var msgTopo = function( txt, classe, tempo )
-{
-    // limpa timeout
-    clearTimeout( msgTopoTimer );
+var msgTopo = function(txt, classe, tempo){
+	if(typeof tempo == 'undefined') 
+		tempo = 5000;
+	else 
+		tempo *= 1000;
 
-    if( $('#msg-topo').length == 0 )
-    {
-        $('body').prepend( '<div><div id="msg-topo"></div></div>' );
-    }
+	clearTimeout(msgTopoTimer);
+	if($('#msg-topo').length == 0)
+			$('body').prepend('<div /><div id="msg-topo" />');
 
-    if ( classe == 'erro' )
-    {
-        $('#msg-topo').removeClass( 'msg' );
-    }
-    else
-    {
-        $('#msg-topo').removeClass( 'erro' );
-    }
+	// hack para IE6 já que não aceita fixed...
+	if ($.browser.msie == true)
+			if($.browser.version < 7)
+					$('#msg-topo').css('top', $(window).scrollTop());
 
-    $('#msg-topo').addClass(classe);
+	$('#msg-topo')
+		.attr('class', '')
+		.addClass(classe)
+		.html(txt)
+		.slideDown( 'fast' )
+		.click(function(e){
+			escondeMsgTopo();
+			e.preventDefault();
+		});
 
-    // hack para IE6 já que não aceita fixed...
-    if ($.browser.msie == true)
-    {
-        if( $.browser.version < 7 )
-        {
-            $('#msg-topo').css( 'top', $(window).scrollTop() );
-        }
-    }
+	msgTopoTimer = setTimeout(escondeMsgTopo, tempo);
+};
 
-    // define texto e exibe msg
-    $('#msg-topo').html( txt );
-    $('#msg-topo').slideDown( 'fast' );
-    $('#msg-topo').click(function(){
-        escondeMsgTopo();
-        return false;
-    });
-
-    // configura timeout
-    msgTopoTimer = setTimeout( escondeMsgTopo, tempo );
-}; // fim msgTopo
-
-var escondeMsgTopo = function()
-{
-    clearTimeout( msgTopoTimer );
-    $('#msg-topo').fadeOut( 'fast' );
+var escondeMsgTopo = function(){
+	clearTimeout(msgTopoTimer);
+	$('#msg-topo').fadeOut('fast');
 };
